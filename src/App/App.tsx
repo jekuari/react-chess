3b6ac4ac68856153ebbdf97def774b0bcabbe559
Ricardo F <ricardo@jekuari.mx>
2023-09-22 23:54:10 -0600

    Removed counter from app.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import ChessBoard from "../Components/Chessboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <ChessBoard />
